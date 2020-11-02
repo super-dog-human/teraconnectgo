a4332f99fcf088e313606e3313fc6c6ed3b591de
SuperDog <super.dog.less.than.human@gmail.com>
2020-11-02 13:34:30 +0900

    add error outputting when token invalid

--- interface/handler/authentication.go
+++ interface/handler/authentication.go
@@ -1,9 +1,11 @@
 package handler
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/pkg/errors"
 	"github.com/super-dog-human/teraconnectgo/domain"
 )
 
@@ -13,6 +15,8 @@ func Authentication() echo.MiddlewareFunc {
 		return func(c echo.Context) error {
 			request := c.Request()
 			if _, err := domain.ValidTokenClaims(request); err != nil {
+				log.Printf("failed to token validation.\n")
+				log.Printf("%v\n", errors.WithStack(err.(error)).Error())
 				return echo.NewHTTPError(http.StatusUnauthorized, "invalid token.")
 			}
 			return next(c)
