48beba34a09b73ff8926ed0b8576939d55898abc
SuperDog <super.dog.less.than.human@gmail.com>
2021-07-31 19:48:02 +0900

    jsonのリクエストボディを構造体にマージする関数を修正し、domainに移動

---
+++ domain/util.go
@@ -0,0 +1,93 @@
+package domain
+
+import (
+	"encoding/json"
+	"reflect"
+	"strings"
+)
+
+func MergeJsonToStruct(jsonDiff *map[string]interface{}, origin interface{}, allowFields *[]string) {
+	allKeys := TopLevelStructKeys(origin)
+
+	o := reflect.ValueOf(origin).Elem()
+	if !o.CanSet() {
+		return
+	}
+
+	for rawName, jsonValue := range *jsonDiff {
+		fieldName := strings.Title(rawName) // jsonのフィールド名を比較用にアッパーキャメルにする
+		if !Contains(&allKeys, fieldName) || !Contains(allowFields, fieldName) {
+			continue
+		}
+
+		jsonFieldType := reflect.TypeOf(jsonValue).String()
+		targetField := o.FieldByName(fieldName)
+
+		if jsonFieldType == "map[string]interface {}" {
+			childJson := jsonValue.(map[string]interface{})
+			switch childTarget := targetField.Interface().(type) {
+			case VoiceSynthesisConfig:
+				allowChildFields := TopLevelStructKeys(&childTarget) // 親フィールドが既に許可されているので子は全て許可
+				MergeJsonToStruct(&childJson, &childTarget, &allowChildFields)
+				targetField.Set(reflect.ValueOf(&childTarget).Elem())
+			}
+		} else if jsonFieldType == "[]interface {}" {
+			switch targets := targetField.Interface().(type) {
+			case []LessonReference:
+				var targetBlankStruct LessonReference
+				allowChildFields := TopLevelStructKeys(&targetBlankStruct)
+				for _, v := range jsonValue.([]interface{}) {
+					child := v.(map[string]interface{})
+					MergeJsonToStruct(&child, &targetBlankStruct, &allowChildFields)
+					targets = append(targets, targetBlankStruct)
+				}
+				targetField.Set(reflect.ValueOf(&targets).Elem()) // 配列は元の値にマージせず丸ごと置き換える
+			}
+		} else {
+			setValueToField(jsonValue, targetField)
+		}
+	}
+}
+
+func setValueToField(jsonValue interface{}, targetField reflect.Value) {
+	originFieldType := targetField.Type().Kind()
+	switch originFieldType {
+	case reflect.Bool:
+		targetField.SetBool(jsonValue.(bool))
+	case reflect.Float32, reflect.Float64:
+		targetField.SetFloat(jsonValue.(float64))
+	case reflect.Int32, reflect.Int64:
+		v := jsonValue.(float64) // jsonのintは全てfloatとして扱われるのでキャストする
+		targetField.SetInt(int64(v))
+	case reflect.String:
+		targetField.SetString(jsonValue.(string))
+	case reflect.Uint64:
+		targetField.SetUint(jsonValue.(uint64))
+	default:
+		// 独自にUnmarshallでenumを定義した型の場合
+		customField := reflect.New(targetField.Type()).Interface()
+		bytes := []byte("\"" + jsonValue.(string) + "\"")
+		json.Unmarshal(bytes, &customField)
+		targetField.Set(reflect.ValueOf(customField).Elem())
+	}
+}
+
+func TopLevelStructKeys(target interface{}) []string {
+	v := reflect.ValueOf(target).Elem()
+
+	var keys []string
+	for i := 0; i < v.NumField(); i++ {
+		keys = append(keys, v.Type().Field(i).Name)
+	}
+
+	return keys
+}
+
+func Contains(s *[]string, e string) bool {
+	for _, a := range *s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
